6f36d30e7f7b11949d5bc9842d82b5d55cf074d4
aquilino <apinto@talentclue.com>
2020-01-24 16:26:04 +0100

    integrated storybooks

---
+++ src/app/components/expense-display/Readme.md
@@ -0,0 +1,8 @@
+#Usage select module
+
+Expense display. 
+
+###Expense
+The select have 3 optional `extraClass`, `id` and  `placeholder`.
+
+
--- src/app/components/expense-display/expense-display.stories.ts
+++ src/app/components/expense-display/expense-display.stories.ts
@@ -1,19 +1,24 @@
 import { ExpenseDisplayComponent } from './expense-display.component';
 import * as moment from 'moment';
-import {registerLocaleData} from "@angular/common";
-import localeEs from "@angular/common/locales/es";
-
-export default { title: 'ExpenseDisplayComponent' }
+import {registerLocaleData} from '@angular/common';
+import localeEs from '@angular/common/locales/es';
+import * as markdown from './Readme.md';
+import { storiesOf } from '@storybook/angular';
 
 registerLocaleData(localeEs);
 
-export const withText = () => ({
-  component: ExpenseDisplayComponent,
-  props: {
-    expense: {
-      amount: 100,
-      date: moment().format(),
-      description: 'Test'
-    },
-  },
-});
+storiesOf('Expense|ExpenseDisplayComponent', module)
+  .add(
+    'Simple Expense Display',
+    () => ({
+      component: ExpenseDisplayComponent,
+      props: {
+        expense: {
+          amount: 100,
+          date: moment().format(),
+          description: 'Test'
+        },
+      },
+    }),
+    { notes: {markdown} }
+  );
